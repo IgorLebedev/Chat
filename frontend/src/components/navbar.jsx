4c24b5ab96091a0b9c1caf02fb82969481bf24bf
igorlebedev <igorlebedev228@gmail.com>
2023-05-25 14:23:36 +0700

    add navbar

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { Container, Navbar, Button } from 'react-bootstrap';
+import AuthContext from '../contexts';
+
+const Navigation = () => {
+  const { isLogged, logOut } = useContext(AuthContext);
+
+  return (
+    <Navbar bg="white" expand="lg" className="shadow-sm">
+      <Container fluid>
+        <Navbar.Brand href="/">Chat</Navbar.Brand>
+        {isLogged && <Button type="button" className="btn btn-dark btn-primary" onClick={() => logOut()}>Выйти</Button>}
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
