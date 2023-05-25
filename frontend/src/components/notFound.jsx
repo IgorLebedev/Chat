ce88a3be6a15638687b08f6471b3cf0145460b6f
igorlebedev <igorlebedev228@gmail.com>
2023-05-25 14:23:49 +0700

    just 404 page

--- frontend/src/components/notFound.jsx
+++ frontend/src/components/notFound.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const NotFound = () => {
-  return (
-    <div>Not Found!</div>
-  );
-};
+const NotFound = () => (
+  <div>Not Found!</div>
+);
 
 export default NotFound;
