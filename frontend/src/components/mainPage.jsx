1fd8844a76ee8bc792d378fe6624c02fd9862c3f
igorlebedev <igorlebedev228@gmail.com>
2023-05-29 22:32:35 +0700

    add modal logic

--- frontend/src/components/mainPage.jsx
+++ frontend/src/components/mainPage.jsx
@@ -10,16 +10,21 @@ import { initChat } from '../slicers/chat.js';
 import Channel from './channel.jsx';
 import AuthContext from '../contexts/authContext.jsx';
 import MessageForm from './messageForm.jsx';
+import NewChannelModal from './newChannelModal.jsx';
 
 const MainPage = () => {
   const [loadingProcess, setLoadingProcess] = useState(null);
+  const [isModalOpen, setModal] = useState(false);
   const { channels, currentChannelId, messages } = useSelector((state) => state.chats);
   const dispatch = useDispatch();
   const { getAuthHeader } = useContext(AuthContext);
 
+  const closeModalHandler = () => {
+    setModal(false);
+  };
+
   useEffect(() => {
     setLoadingProcess('loading');
-    console.log(getAuthHeader());
     const getData = async () => {
       try {
         const res = await axios.get(routes.data(), getAuthHeader());
@@ -38,7 +43,7 @@ const MainPage = () => {
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
           <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
             <b>Каналы</b>
-            <button type="button" className="p-0 text-primary btn btn-group-vertical">
+            <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => setModal(true)}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
@@ -81,6 +86,7 @@ const MainPage = () => {
           </div>
         </div>
       </div>
+      {isModalOpen && <NewChannelModal closeHandler={closeModalHandler} />}
     </div>
   );
 };
