b8b6892cc07bbda16c1c3845e8bd8304feb67ce3
igorlebedev <igorlebedev228@gmail.com>
2023-05-29 17:38:48 +0700

    separate auth provider

---
+++ frontend/src/providers/authProvider.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import AuthContext from '../contexts/authContext.jsx';
+
+const AuthProvider = ({ children }) => {
+  const localStorageUser = JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = useState(localStorageUser || null);
+
+  const logIn = (userData) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    setUser(userData);
+  };
+  const logOut = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
+  const getAuthHeader = () => ({ headers: { Authorization: `Bearer ${user.token}` } });
+
+  return (
+    <AuthContext.Provider value={{
+      getAuthHeader,
+      user,
+      logIn,
+      logOut,
+    }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
