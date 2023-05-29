d0f7038150a255ffaff23d1b29205f75fe75abea
igorlebedev <igorlebedev228@gmail.com>
2023-05-29 17:36:50 +0700

    add socket provider and minor fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,64 +4,42 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import NotFound from './components/notFound.jsx';
 import MainPage from './components/mainPage.jsx';
 import Login from './components/login.jsx';
-import AuthContext from './contexts/index.jsx';
 import Navigation from './components/navbar.jsx';
-
-const AuthProvider = ({ children }) => {
-  const [isLogged, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (localStorage.token) {
-      setLoggedIn(true);
-    }
-  }, []);
-
-  const logIn = () => {
-    setLoggedIn(true);
-  };
-  const logOut = () => {
-    localStorage.removeItem('token');
-    setLoggedIn(false);
-  };
-
-  return (
-    <AuthContext.Provider value={{ isLogged, logIn, logOut }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+import AuthProvider from './providers/authProvider.jsx';
+import SocketProvider from './providers/socketProvider.jsx';
 
 const MainRoute = ({ children }) => {
-  const { token } = localStorage;
+  const { user } = localStorage;
   return (
-    token ? children : <Navigate to="/login" />
+    user ? children : <Navigate to="/login" />
   );
 };
 
 const App = () => (
-  <AuthProvider>
-    <div className="d-flex flex-column h-100">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route
-            path="/"
-            element={(
-              <MainRoute>
-                <MainPage />
-              </MainRoute>
-            )}
-          />
-          <Route path="*" element={<NotFound />} />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  </AuthProvider>
+  <SocketProvider>
+    <AuthProvider>
+      <div className="d-flex flex-column h-100">
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route
+              path="/"
+              element={(
+                <MainRoute>
+                  <MainPage />
+                </MainRoute>
+              )}
+            />
+            <Route path="*" element={<NotFound />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
+  </SocketProvider>
 );
 
 export default App;
