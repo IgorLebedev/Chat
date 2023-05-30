83e01371d5b456b03fbe5a8c2132e6b6eb48b432
igorlebedev <igorlebedev228@gmail.com>
2023-05-30 22:05:03 +0700

    add remove channel modal

---
+++ frontend/src/components/modals/removeChannelModal.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import SocketContext from '../../contexts/socketContext';
+import { setCurrentEditingChannel } from '../../slicers/chat';
+
+const RemoveChannelModal = ({ closeHandler }) => {
+  const { currentEditingId } = useSelector((state) => state.chats);
+  const dispatch = useDispatch();
+  const { sendRemovedChannel } = useContext(SocketContext);
+  const removeHandler = () => {
+    sendRemovedChannel({ id: currentEditingId });
+    closeHandler();
+    dispatch(setCurrentEditingChannel(null));
+  };
+  return (
+    <Modal show onHide={() => closeHandler()}>
+      <Modal.Header closeButton>
+        <Modal.Title>Удалить канал</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p className="lead">Уверены?</p>
+        <div className="d-flex justify-content-end">
+          <Button type="button" variant="secondary" className="me-2" onClick={() => closeHandler()}>Отменить</Button>
+          <Button type="submit" variant="primary" onClick={() => removeHandler()}>Удалить</Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default RemoveChannelModal;
