3a73ddad3e36166c5fc2a9845d44705d8875ae63
igorlebedev <igorlebedev228@gmail.com>
2023-05-30 22:03:57 +0700

    add remove and rename modals

--- frontend/src/components/mainPage.jsx
+++ frontend/src/components/mainPage.jsx
@@ -7,20 +7,38 @@ import React, {
 import axios from 'axios';
 import routes from '../routes/routes.js';
 import { initChat } from '../slicers/chat.js';
-import Channel from './channel.jsx';
+import Channels from './channels.jsx';
 import AuthContext from '../contexts/authContext.jsx';
 import MessageForm from './messageForm.jsx';
-import NewChannelModal from './newChannelModal.jsx';
+import NewChannelModal from './modals/newChannelModal.jsx';
+import RemoveChannelModal from './modals/removeChannelModal.jsx';
+import RenameChannelModal from './modals/renameChannelModal.jsx';
 
 const MainPage = () => {
   const [loadingProcess, setLoadingProcess] = useState(null);
-  const [isModalOpen, setModal] = useState(false);
+  const [isNewChannelModalOpened, setNewChannelModal] = useState(false);
+  const [isRemoveChannelModalOpened, setRemoveChannelModal] = useState(false);
+  const [isRenameChannelModalOpened, setRenameChannelModal] = useState(false);
   const { channels, currentChannelId, messages } = useSelector((state) => state.chats);
   const dispatch = useDispatch();
   const { getAuthHeader } = useContext(AuthContext);
 
-  const closeModalHandler = () => {
-    setModal(false);
+  const modalHandlers = {
+    openRemoveChannel: () => {
+      setRemoveChannelModal(true);
+    },
+    openRenameChannel: () => {
+      setRenameChannelModal(true);
+    },
+    closeNewChannel: () => {
+      setNewChannelModal(false);
+    },
+    closeRemoveChannel: () => {
+      setRemoveChannelModal(false);
+    },
+    closeRenameChannel: () => {
+      setRenameChannelModal(false);
+    },
   };
 
   useEffect(() => {
@@ -43,7 +61,7 @@ const MainPage = () => {
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
           <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
             <b>Каналы</b>
-            <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => setModal(true)}>
+            <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => setNewChannelModal(true)}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
@@ -51,11 +69,12 @@ const MainPage = () => {
               <span className="visually-hidden">+</span>
             </button>
           </div>
-          <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-            {loadingProcess === 'loaded' && channels.map(({ id, name }) => (
-              <Channel key={id} id={id} name={name} />
-            ))}
-          </ul>
+          <Channels
+            loadingProcess={loadingProcess}
+            channels={channels}
+            openRenameModal={modalHandlers.openRenameChannel}
+            openRemoveModal={modalHandlers.openRemoveChannel}
+          />
         </div>
         <div className="col p-0 h-100">
           <div className="d-flex flex-column h-100">
@@ -86,7 +105,11 @@ const MainPage = () => {
           </div>
         </div>
       </div>
-      {isModalOpen && <NewChannelModal closeHandler={closeModalHandler} />}
+      {isNewChannelModalOpened && <NewChannelModal closeHandler={modalHandlers.closeNewChannel} />}
+      {isRemoveChannelModalOpened && (
+      <RemoveChannelModal closeHandler={modalHandlers.closeRemoveChannel} />)}
+      {isRenameChannelModalOpened && (
+      <RenameChannelModal closeHandler={modalHandlers.closeRenameChannel} />)}
     </div>
   );
 };
