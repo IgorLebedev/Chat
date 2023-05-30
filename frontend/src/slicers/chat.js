b785facc989c9cebee1a2a0aed1c374a85bd29ad
igorlebedev <igorlebedev228@gmail.com>
2023-05-30 22:05:59 +0700

    add remove rename & current editing channel

--- frontend/src/slicers/chat.js
+++ frontend/src/slicers/chat.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
+const DEFAULT_CHANNEL_ID = 1;
+
 const initialState = {
   channels: [],
   currentChannelId: null,
+  currentEditingId: null,
   messages: [],
 };
 
@@ -26,6 +29,18 @@ const chatReducer = createSlice({
     addChannel: (state, { payload }) => {
       state.channels.push(payload);
     },
+    removeChannel: (state, { payload }) => {
+      state.channels = state.channels.filter(({ id }) => id !== payload.id);
+      state.messages = state.messages.filter(({ channelId }) => channelId !== payload.id);
+      state.currentChannelId = DEFAULT_CHANNEL_ID;
+    },
+    renameChannel: (state, { payload }) => {
+      const desiredChannel = state.channels.find(({ id }) => payload.id === id);
+      desiredChannel.name = payload.name;
+    },
+    setCurrentEditingChannel: (state, { payload }) => {
+      state.currentEditingId = payload;
+    },
   },
 });
 
@@ -34,6 +49,9 @@ export const {
   changeChat,
   addMessage,
   addChannel,
+  removeChannel,
+  setCurrentEditingChannel,
+  renameChannel,
 } = chatReducer.actions;
 
 export default chatReducer.reducer;
