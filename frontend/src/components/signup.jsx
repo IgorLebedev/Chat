9105f6f34f19459417f15aac5e66784f6a16c22b
igorlebedev <igorlebedev228@gmail.com>
2023-05-31 17:52:12 +0700

    add signup page

---
+++ frontend/src/components/signup.jsx
@@ -0,0 +1,137 @@
+import React, {
+  useContext, useRef, useEffect, useState,
+} from 'react';
+import {
+  Container, Row, Col, Form, Card, Button,
+} from 'react-bootstrap';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import routes from '../routes/routes';
+import AuthContext from '../contexts/authContext';
+
+const signUpSchema = Yup.object({
+  username: Yup
+    .string()
+    .min(3, 'От 3 до 20 символов')
+    .max(20, 'От 3 до 20 символов')
+    .required('Обязательное поле'),
+  password: Yup
+    .string()
+    .min(6, 'Не менее 6 символов')
+    .required('Обязательное поле'),
+  passwordConfirm: Yup
+    .string()
+    .oneOf([Yup.ref('password'), null], 'Пароли должны совпадать'),
+});
+
+const SignUp = () => {
+  const { logIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [signUpProcess, setProcess] = useState(null);
+  const usernameInput = useRef(null);
+  useEffect(() => {
+    usernameInput.current.focus();
+  }, []);
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      passwordConfirm: '',
+      isValidName: '',
+    },
+    validationSchema: signUpSchema,
+    onSubmit: async ({ username, password }) => {
+      setProcess('signingUp');
+      try {
+        const { data: { token } } = await axios.post(routes.signup(), { username, password });
+        logIn({ username, token });
+        setProcess('success');
+        navigate('/');
+      } catch (error) {
+        setProcess('error');
+        if (error.response.status === 409) {
+          formik.setFieldError('isValidName', 'Такой пользователь уже существует');
+        } else {
+          console.warn(error);
+        }
+      }
+    },
+  });
+  useEffect(() => {
+    console.log(formik.errors);
+  }, [formik.errors]);
+  return (
+    <Container fluid className="h-100">
+      <Row className="row justify-content-center align-content-center h-100">
+        <Col className="col-12 col-md-8 col-xxl-4">
+          <Card className="shadow-sm">
+            <Card className="flex-column flex-md-row justify-content-around align-items-center p-5">
+              <Form className="w-75" onSubmit={formik.handleSubmit}>
+                <h1 className="text-center mb-4">Регистрация</h1>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    type="username"
+                    className="form-control"
+                    id="username"
+                    ref={usernameInput}
+                    onBlur={formik.handleBlur}
+                    isInvalid={formik.touched.username && formik.errors.username}
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    placeholder="username"
+                  />
+                  <Form.Label htmlFor="username">Имя пользователя</Form.Label>
+                  <Form.Control.Feedback className="invalid-tooltip" type="invalid">
+                    {formik.errors.username}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    onBlur={formik.handleBlur}
+                    isInvalid={formik.touched.password && formik.errors.password}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    placeholder="password"
+                  />
+                  <Form.Label htmlFor="password">Пароль</Form.Label>
+                  <Form.Control.Feedback className="invalid-tooltip" type="invalid">
+                    {formik.errors.password}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    type="password"
+                    className="form-control"
+                    id="passwordConfirm"
+                    onBlur={formik.handleBlur}
+                    isInvalid={formik.touched.passwordConfirm && formik.errors.passwordConfirm}
+                    onChange={formik.handleChange}
+                    value={formik.values.passwordConfirm}
+                    placeholder="passwordConfirm"
+                  />
+                  <Form.Label htmlFor="passwordConfirm">Пароль</Form.Label>
+                  <Form.Control.Feedback className="invalid-tooltip" type="invalid">
+                    {formik.errors.passwordConfirm}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                {formik.errors.isValidName && Object.keys(formik.errors).length > 0 && (
+                  <Form.Control.Feedback className="invalid-tooltip" type="invalid">
+                    Ошибка формы. Проверьте введенные данные.
+                  </Form.Control.Feedback>
+                )}
+                <Button type="submit" variant="outline-dark w-100" disabled={signUpProcess === 'signingUp'}>Зарегистрироваться</Button>
+              </Form>
+            </Card>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignUp;
