33a3669fa3e94c7300a65424e9aa365732e9df65
igorlebedev <igorlebedev228@gmail.com>
2023-05-31 17:51:55 +0700

    rework login page

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import React, {
-  useState,
-  useEffect,
-  useRef,
-  useContext,
+  useState, useEffect, useRef, useContext,
 } from 'react';
 import { useFormik } from 'formik';
 import { useNavigate } from 'react-router-dom';
-import { Button, Form } from 'react-bootstrap';
+import {
+  Button, Container, Form, Row, Col, Card,
+} from 'react-bootstrap';
 import routes from '../routes/routes.js';
 import AuthContext from '../contexts/authContext.jsx';
 
@@ -25,12 +24,12 @@ const Login = () => {
       username: '',
       password: '',
     },
-    onSubmit: async (values) => {
+    onSubmit: async ({ username, password }) => {
       setProcess('logging');
       try {
-        const { data: { token } } = await axios.post(routes.login(), values);
+        const { data: { token } } = await axios.post(routes.login(), { username, password });
         setProcess('success');
-        logIn({ username: values.username, token });
+        logIn({ username, token });
         navigate('/');
       } catch (error) {
         setProcess('error');
@@ -39,43 +38,57 @@ const Login = () => {
   });
 
   return (
-    <div className="container-fluid">
-      <Form onSubmit={formik.handleSubmit}>
-        <h1>Войти</h1>
-        <Form.Group className="form-floating mb-3">
-          <Form.Control
-            type="text"
-            className="form-control"
-            id="username"
-            ref={inputEl}
-            required
-            onChange={formik.handleChange}
-            value={formik.values.username}
-            isInvalid={loginProcess === 'error'}
-            placeholder="username"
-          />
-          <Form.Label htmlFor="username">Ваш ник</Form.Label>
-        </Form.Group>
-        <Form.Group className="form-floating mb-4">
-          <Form.Control
-            type="password"
-            className="form-control"
-            id="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            isInvalid={loginProcess === 'error'}
-            placeholder="password"
-          />
-          <Form.Label htmlFor="password">Пароль</Form.Label>
-          <Form.Control.Feedback className="invalid-tooltip" type="invalid">
-            Неверные имя пользователя или пароль
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button type="submit" className="w-100 btn btn-dark" disabled={loginProcess === 'logging'}>
-          Войти
-        </Button>
-      </Form>
-    </div>
+    <Container fluid className="h-100">
+      <Row className="row justify-content-center align-content-center h-100">
+        <Col className="col-12 col-md-8 col-xxl-4">
+          <Card>
+            <Card.Body className="flex-column body row p-5">
+              <Form onSubmit={formik.handleSubmit}>
+                <h1 className="text-center mb-4">Войти</h1>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    ref={inputEl}
+                    required
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    isInvalid={loginProcess === 'error'}
+                    placeholder="username"
+                  />
+                  <Form.Label htmlFor="username">Ваш ник</Form.Label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    isInvalid={loginProcess === 'error'}
+                    placeholder="password"
+                  />
+                  <Form.Label htmlFor="password">Пароль</Form.Label>
+                  <Form.Control.Feedback className="invalid-tooltip" type="invalid">
+                    Неверные имя пользователя или пароль
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Button type="submit" className="w-100 btn btn-dark" disabled={loginProcess === 'logging'}>
+                  Войти
+                </Button>
+              </Form>
+            </Card.Body>
+            <Card.Footer>
+              <div className="text-center">
+                <span>Нет аккаунта?</span>
+                <a href="/signup">Регистрация</a>
+              </div>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
