887483318a67576bb48a12e9c5c974652c4ea678
igorlebedev <igorlebedev228@gmail.com>
2023-06-01 18:26:05 +0700

    i18n init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import './i18n';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
