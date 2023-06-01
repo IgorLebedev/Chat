a91718799e787e3bf8f1543742933733beb0add3
igorlebedev <igorlebedev228@gmail.com>
2023-06-01 18:29:19 +0700

    rework not found page and use i18n

--- frontend/src/components/notFound.jsx
+++ frontend/src/components/notFound.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const NotFound = () => (
-  <div>Not Found!</div>
-);
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="text-center">
+      <h1 className="h4 text-muted">{t('notFound.title')}</h1>
+      <p className="text-muted">
+        {t('notFound.redirect')}
+        {' '}
+        <Link to="/">{t('notFound.mainPage')}</Link>
+      </p>
+    </div>
+  );
+};
 
 export default NotFound;
