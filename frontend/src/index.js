6ca24f7626b4504ed1a81969f3dbe949f3c25f0c
igorlebedev <igorlebedev228@gmail.com>
2023-06-02 18:08:32 +0700

    minor rework and toastify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,21 @@ import { Provider } from 'react-redux';
 import './i18n';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import store from './slicers/store';
+import AuthProvider from './providers/AuthProvider.jsx';
+import SocketProvider from './providers/SocketProvider.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <SocketProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </SocketProvider>
     </Provider>
   </React.StrictMode>,
 );
