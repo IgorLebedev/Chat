678f5b5d4593890a065faad6fb6130ecaec73855
igorlebedev <igorlebedev228@gmail.com>
2023-06-02 18:08:14 +0700

    minor rework & add toastify container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,12 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
-import NotFound from './components/notFound.jsx';
-import MainPage from './components/mainPage.jsx';
-import Login from './components/login.jsx';
-import Navigation from './components/navbar.jsx';
-import SignUp from './components/signup.jsx';
-import AuthProvider from './providers/authProvider.jsx';
-import SocketProvider from './providers/socketProvider.jsx';
+import { ToastContainer } from 'react-toastify';
+import NotFound from './components/NotFoundPage.jsx';
+import MainPage from './components/MainPage.jsx';
+import Login from './components/LoginPage.jsx';
+import Navigation from './components/Navbar.jsx';
+import SignUp from './components/SignUpPage.jsx';
 
 const MainRoute = ({ children }) => {
   const { user } = localStorage;
@@ -20,28 +19,36 @@ const MainRoute = ({ children }) => {
 };
 
 const App = () => (
-  <SocketProvider>
-    <AuthProvider>
-      <div className="d-flex flex-column h-100">
-        <BrowserRouter>
-          <Navigation />
-          <Routes>
-            <Route
-              path="/"
-              element={(
-                <MainRoute>
-                  <MainPage />
-                </MainRoute>
-              )}
-            />
-            <Route path="*" element={<NotFound />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<SignUp />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </AuthProvider>
-  </SocketProvider>
+  <div className="d-flex flex-column h-100">
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <MainRoute>
+              <MainPage />
+            </MainRoute>
+          )}
+        />
+        <Route path="*" element={<NotFound />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  </div>
 );
 
 export default App;
