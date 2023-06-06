34c6146bdc8300b09854953fbc919b93ffb0c319
igorlebedev <igorlebedev228@gmail.com>
2023-06-06 19:14:40 +0700

    remove i18n import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './i18n';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
