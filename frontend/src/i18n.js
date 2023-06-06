aa9e0c225ee7cfcf883c5a4d83b1402a2fc23222
igorlebedev <igorlebedev228@gmail.com>
2023-06-06 19:14:30 +0700

    rework make instance

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -2,15 +2,19 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from './locales/index.js';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'ru',
-    debug: true,
-    resources,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+const initI18n = () => {
+  const i18nInst = i18n.createInstance();
+  i18nInst
+    .use(initReactI18next)
+    .init({
+      fallbackLng: 'ru',
+      debug: false,
+      resources,
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+  return i18nInst;
+};
 
-export default i18n;
+export default initI18n;
