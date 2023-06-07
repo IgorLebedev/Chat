40199ee10f3d4baa3dd1bde0966a09798c668559
igorlebedev <igorlebedev228@gmail.com>
2023-06-07 17:11:18 +0700

    rework modal logic

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import React, {
   useEffect,
   useState,
@@ -17,10 +17,8 @@ import SpinnerComponent from './Spinner.jsx';
 const MainPage = () => {
   const [loadingProcess, setLoadingProcess] = useState(null);
   const { fetchData } = useContext(AuthContext);
+  const { currentModal } = useSelector((state) => state.modals);
   const dispatch = useDispatch();
-  const [modal, setModal] = useState(null);
-
-  const closeModal = () => setModal(null);
 
   useEffect(() => {
     setLoadingProcess('loading');
@@ -43,11 +41,8 @@ const MainPage = () => {
       <div className="container h-100 my-4 overflow-hidden rounded shadow">
         <div className="row h-100 bg-white flex-md-row">
           <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-            <ChannelsHeader openNewChannelModal={() => setModal('newChannel')} />
-            <Channels
-              openRenameModal={() => setModal('rename')}
-              openRemoveModal={() => setModal('remove')}
-            />
+            <ChannelsHeader />
+            <Channels />
           </div>
           <div className="col p-0 h-100">
             <div className="d-flex flex-column h-100">
@@ -57,7 +52,7 @@ const MainPage = () => {
             </div>
           </div>
         </div>
-        {modal && getModal(modal, closeModal)}
+        {currentModal && getModal(currentModal)}
       </div>
       )}
     </>
